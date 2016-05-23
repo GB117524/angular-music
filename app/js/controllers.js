3e48533c86dcab221b0ae58190266e5d5344115d
kudos72 <kirk.narey@gmail.com>
2016-05-23 08:31:42 +0100

    Friday efforts.
    Long poll to Last.FM work out currently playing song

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,41 +1,29 @@
+/* Controllers */
+
 'use strict';
 
 var musicApp = angular.module('musicApp', []);
 
-
-
-// musicApp.controller("bandListCtrl", function($scope){
+// var bands = { artists : {
+//     items: [
+//         {name: 'Metallica'},
+//         {name: 'Megadeth'},
+//         {name: 'Royal Blood'}]
 //
-//     $scope.bands = [
-//         {'name': 'Metallica',
-//         'genre': 'thrash'},
-//         {'name': 'Royal Blood',
-//             'genre': 'rock'},
-//         {'name': 'Prodegy',
-//             'genre': 'dance'},
-//         {'name': 'Megadeth',
-//             'genre': 'thrash'}
-//     ];
-//
-//
-//
-// });
+// }
+//};
 
-var bands = { artists : {
-    items: [
-        {name: 'Metallica'},
-        {name: 'Megadeth'},
-        {name: 'Royal Blood'}]
-
-}
-
-};
+var bands = {};
 
 musicApp.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }
 ]);
+
+musicApp.run(function($rootScope){
+   $rootScope.nowPlaying = {'artist' : {'#text' : 'Cool and the Gang'}};
+});
 
 musicApp.controller("SearchController", function($scope, $http){
 
@@ -74,4 +62,56 @@ musicApp.controller("AlertController", function(){
         window.alert("ALERT!");
     };
 });
-/* Controllers */
+
+musicApp.controller("LastFmController", function($scope, $rootScope, $timeout, $http) {
+
+
+//     this.nowPlayingManual = function () {
+//         var apiKey = "f3b3f5f21a179582edf96ff2269d5754";
+//     var user = "KingKudos";
+//     var query = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json';
+//     $http.get(query).success(function (response) {
+//         // window.alert(response.recenttracks.track[0].artist['#text']);
+//         $rootScope.nowPlaying = response.recenttracks.track[0];
+//         // window.alert($rootScope.nowPlaying.artist['#text']);
+//     });
+// }
+    var nowPlayingPoll = function(){
+            $timeout(function() {
+                var apiKey = "f3b3f5f21a179582edf96ff2269d5754";
+                var user = "KingKudos";
+                var query = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json';
+                $http.get(query).success(function(response) {
+                    // window.alert(response.recenttracks.track[0].artist['#text']);
+                    $rootScope.value = response.recenttracks.track[0];
+                    // window.alert($rootScope.nowPlaying.artist['#text']);
+                });
+
+                nowPlayingPoll();
+            }, 1000);
+        };
+        nowPlayingPoll();
+
+
+
+
+});
+
+musicApp.controller('Ctrl1', function($scope, $rootScope, $timeout, $http) {
+    $scope.value = 1;
+    var poll = function() {
+        $timeout(function() {
+            var apiKey = "f3b3f5f21a179582edf96ff2269d5754";
+            var user = "KingKudos";
+            var query = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json';
+            $http.get(query).success(function(response) {
+                $scope.value = response.recenttracks.track[0].artist['#text'];
+                $rootScope.nowPlaying = response.recenttracks.track[0];
+            });
+
+            poll();
+        }, 1000);
+    };
+    poll();
+});
+
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,15 @@
 'use strict';
 
 /* Directives */
+
+var musicApp = angular.module('musicDirectives', []);
+
+musicApp.directive('chart', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            value: '=' // '=' indicates 2 way binding
+        },
+        template: "<div> Now Playing : {{value}} </div>"
+    };
+});
