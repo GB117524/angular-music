c4c075e224307b557321d104a136bb800164a8af
Kirk <kirk.narey@uk.ibm.com>
2016-05-24 18:29:19 +0100

    Youtube videos and now playing watch added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,17 +4,6 @@
 
 var musicApp = angular.module('musicApp', ['jtt_youtube']);
 
-// var bands = { artists : {
-//     items: [
-//         {name: 'Metallica'},
-//         {name: 'Megadeth'},
-//         {name: 'Royal Blood'}]
-//
-// }
-//};
-
-var bands = {};
-
 musicApp.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
@@ -22,14 +11,14 @@ musicApp.config(['$httpProvider', function($httpProvider) {
 ]);
 
 musicApp.run(function($rootScope){
-   $rootScope.nowPlaying = {'artist' : {'#text' : 'Cool and the Gang'}};
+    //$rootScope.nowPlaying = {'artist' : {'#text' : 'Cool and the Gang'}};
+    $rootScope.nowPlaying = {};
 });
 
 musicApp.controller("SearchController", function($scope, $http){
 
     var searchCtrl = this;
-    searchCtrl.searchResults = bands;
-
+    searchCtrl.searchResults = {};
     searchCtrl.searchString = "";
     searchCtrl.searchHistory = [];
 
@@ -38,7 +27,7 @@ musicApp.controller("SearchController", function($scope, $http){
     var query = 'http://api.spotify.com/v1/search?q=' + this.searchString + '&type=artist';
 
      $http.get(query).success(function(response){
-        window.alert(response.artists.items[5].name);
+        //window.alert(response.artists.items[5].name);
          searchCtrl.searchResults = response;
          searchCtrl.searchHistory.push(searchCtrl.searchString);
          searchCtrl.searchString = "";
@@ -47,16 +36,22 @@ musicApp.controller("SearchController", function($scope, $http){
 
 });
 
-musicApp.controller("YouTubeSearch", function ($scope,$http, $rootScope)
+musicApp.controller("YouTubeSearch", function ($scope,$http, $rootScope, youtubeFactory)
 {
     var apiKey= 'AIzaSyAFJlvdxhCrCBdk_gCGM8xMaChU2d_weaM';
+    var youTubeResult = this;
+    youTubeResult.searchResults = {};
+    $scope.searchResults = {};
     //docs: https://developers.google.com/youtube/v3/docs/search/list
+    //window.alert($rootScope.nowPlaying.artist['#text']);
+    $rootScope.$watch('nowPlaying', function()
+    {
     youtubeFactory.getVideosFromSearchByParams({
         q: $rootScope.nowPlaying.artist['#text'], // (optional) search string
         //location: "<SEARCH_LOCATION>", // (optional) The parameter value is a string that specifies latitude/longitude coordinates e.g. '37.42307,-122.08427'.
         //locationRadius: "<LOCATION_RADIUS>", // (optional) valid values e.g. '1500m', '5km', '10000ft', and '0.75mi' | default: '5000m'
         order: "relevance", // (optional) valid values: 'date', 'rating', 'relevance', 'title', 'videoCount', 'viewCount' | default: 'date'
-        //maxResults: "<MAX_RESULTS>", // (optional) valid values: 0-50 | default: 5
+        maxResults: "6",//"<MAX_RESULTS>", // (optional) valid values: 0-50 | default:
         //publishedAfter: "<PUBLISHED_AFTER>", // (optional) RFC 3339 formatted date-time value (1970-01-01T00:00:00Z)
         //publishedBefore: "<PUBLISHED_AFTER>", // (optional) RFC 3339 formatted date-time value (1970-01-01T00:00:00Z)
         //regionCode: "<REGION_CODE>", // (optional) ISO 3166-1 alpha-2 country code
@@ -68,70 +63,43 @@ musicApp.controller("YouTubeSearch", function ($scope,$http, $rootScope)
         //fields: "<FIELDS>", // (optional) Selector specifying which fields to include in a partial response
         //pageToken: "<PAGE_TOKEN>", // (optional)
         //part: "<PART>", // (optional) default: 'id,snippet'
-        key: apiKey,
+        key: apiKey
     }).then(function (_data) {
         //on success
-        var returnString = '<img src=\"' + _data.items[0].snippet.thumbnails.high.url + '\" alt=\"' + _data.items[0].snippet.title + '">';
-        return returnString;
+        $scope.searchResults = _data.data;
+        youTubeResult.searchResults = _data.data;
 
     }).catch(function (_data) {
         //on error
-    });
-
-});
-
-musicApp.controller("Hello", function($scope,$http)
-{
-    $http.get('http://rest-service.guides.spring.io/greeting')
-        .success(function(data) {
-            $scope.greeting = data;
-        });
-
-});
-
-musicApp.controller("AlertController", function(){
-    this.alert = function()
-    {
-        window.alert("ALERT!");
-    };
-});
-
-musicApp.controller("LastFmController", function($scope, $rootScope, $timeout, $http) {
-
-    var nowPlayingPoll = function(){
-            $timeout(function() {
-                var apiKey = "f3b3f5f21a179582edf96ff2269d5754";
-                var user = "KingKudos";
-                var query = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json';
-                $http.get(query).success(function(response) {
-                    // window.alert(response.recenttracks.track[0].artist['#text']);
-                    $rootScope.value = response.recenttracks.track[0];
-                    // window.alert($rootScope.nowPlaying.artist['#text']);
-                });
-
-                nowPlayingPoll();
-            }, 1000);
-        };
-        nowPlayingPoll();
-
-
+    }
+    )});
 
 
 });
-
-
 
 musicApp.controller('NowPlayingUpdate', function($scope, $rootScope, $timeout, $http) {
-    $scope.value = 1;
-    var poll = function() {
-        $timeout(function() {
+    var poll;
+    poll = function () {
+        $timeout(function () {
             var apiKey = "f3b3f5f21a179582edf96ff2269d5754";
             var user = "KingKudos";
             var query = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + user + '&api_key=' + apiKey + '&format=json';
-            $http.get(query).success(function(response) {
-                $scope.value = response.recenttracks.track[0].artist['#text'];
-                $rootScope.nowPlaying = response.recenttracks.track[0];
-            });
+            $scope.loading = true;
+            $http.get(query).success(function (response) {
+                if (JSON.stringify($rootScope.nowPlaying) !== JSON.stringify(response.recenttracks.track[0])) {
+                    //$scope.value = response.recenttracks.track[0].artist['#text'];
+                    $rootScope.nowPlaying = response.recenttracks.track[0];
+                }
+
+            })
+                .catch(function () {
+                    //error condition
+                    window.alert("Can't receive now playing information");
+                })
+                .finally(function () {
+                    //stop the spinner
+                    $scope.loading = false;
+                });
 
             poll();
         }, 1000);
